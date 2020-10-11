6f868dd0a41f3b177cc1c6332c1e46203824c92c
Andrew Hall <ryzngard@live.com>
2020-10-11 16:41:29 -0700

    Add console application

---
+++ src/MathUtilsConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MathUtilsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i} is even? {IsEven(i)}");
+            }
+        }
+
+        static bool IsEven(int i) => DotNot.Math.MathUtils.IsEven(i);
+    }
+}
